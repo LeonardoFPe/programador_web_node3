768890cbdbb57aac3ec4fad7425e7a5394baee2f
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2025-07-11 18:47:57 -0300

    aula 11/07

--- 07_npm/index.js
+++ 07_npm/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
-
 const port = 3001
 
+const path = require('path')
+
+const basePath = path.join(__dirname, 'templates')
+
 app.get('/', (req, res) =>{
-    res.send('Olá Mundo!')
+   res.sendFile(`${basePath}/index.html`)
 })
 
 app.listen(port, () => {
