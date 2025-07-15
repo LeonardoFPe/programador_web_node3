ff0b5d2f83b39addc23c78f9f2dc87bcac8301b1
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2025-07-15 19:54:36 -0300

    15/07

--- alfala_books/src/App.js
+++ alfala_books/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import Logo from './components/Logo';
-import perfil from './img/perfil.svg'
-import sacola from './img/sacola.svg'
+
 import OptionsHeader from './components/OptionsHeader/index.js';
+import IconesHeader from './components/IconesHeader/index.js';
 
 
-const icones = [perfil, sacola]
 
 function App() {
   return (
@@ -13,13 +12,7 @@ function App() {
       <header className="App-header">
         <Logo />
         <OptionsHeader />
-        <ul className='icones'>
-          { icones.map ((icone) => (
-            <li className='icone'>
-              <img src={icone}></img>
-            </li>
-          ))}
-        </ul>
+        <IconesHeader />
       </header>
     </div>
   );
