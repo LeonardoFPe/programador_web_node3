c6564658970bd360fba2446de241044dffc1b2e5
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2025-07-17 20:18:58 -0300

    20:18

--- alfala_books/src/App.js
+++ alfala_books/src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
 import Header from './components/Header';
 import styled from 'styled-components';
+import Pesquisa from './components/Pesquisa';
 
 
 const AppContainer = styled.div`
@@ -17,6 +17,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Pesquisa />
       < AppContainer />
     </div>
   );
---
+++ alfala_books/src/components/Pesquisa/index.js
@@ -0,0 +1,14 @@
+
+
+
+function Pesquisa() {
+    return(
+        <section>
+            <h2>Já sabe por onde começar?</h2>
+            <h3>Encontre seu livro em nossa estante</h3>
+            <input placeholder="Escreva sua próxima leitura" />
+        </section>
+    )
+}
+
+export default Pesquisa;
