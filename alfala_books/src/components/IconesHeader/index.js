04716a5d92c6d7d8b9eb159071ce46ac068b9046
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2025-07-17 19:19:05 -0300

    17-07

--- alfala_books/src/components/IconesHeader/index.js
+++ alfala_books/src/components/IconesHeader/index.js
@@ -1,20 +1,28 @@
 import './styles.css'
-
+import styled from 'styled-components'
 import perfil from '../../img/perfil.svg'
 import sacola from '../../img/sacola.svg'
 
 
-const icones = [perfil, sacola]
+const Icone = styled.li`
+    margin-right: 40px;
+    width: 25px;
+`
+const Icones = styled.li`
+    display: flex;
+    align-items: center;
+`
 
+const icones = [perfil, sacola]
 function IconesHeader() {
     return (
-        <ul className='icones'>
+        <Icones>
             {icones.map((icone) => (
-                <li className='icone'>
+                <Icone>
                     <img src={icone}></img>
-                </li>
+                </Icone>
             ))}
-        </ul>
+        </Icones>
     )
 }
 
