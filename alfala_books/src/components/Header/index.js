9d80c9025e94de8b1b9bd91c4f3629f51fa6176c
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2025-07-16 22:18:27 -0300

    16-07

--- alfala_books/src/components/Header/index.js
+++ alfala_books/src/components/Header/index.js
@@ -1,16 +1,26 @@
-import './style.css'
+
 import Logo from "../Logo";
 import OptionsHeader from "../OptionsHeader";
 import IconesHeader from "../IconesHeader";
+import styled from 'styled-components'
+
+
+const HeaderContainer = styled.header`
+    background-color: #FFF;
+    display: flex;
+    justify-content: center;
+`
 
 
 
 function Header() {
     return (
         <header className="App-header">
-            <Logo />
-            <OptionsHeader />
-            <IconesHeader />
+            <HeaderContainer >
+                <Logo />
+                <OptionsHeader />
+                <IconesHeader />
+            </HeaderContainer>
         </header>
     )
 }
