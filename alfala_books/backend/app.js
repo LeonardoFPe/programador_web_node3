14bed421c525aa88d1ff54db03295d5cc28814f8
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2025-07-24 22:08:33 -0300

    finalizado aula 24-07

--- alfala_books/backend/app.js
+++ alfala_books/backend/app.js
@@ -1,10 +1,14 @@
 import express from 'express'
+import routerLivros from './routes/livrosRouter.js'
+
 const app = express()
+app.use(express.json())
+
 const port = 8000
 
-app.get('/', (req, res) => {
-    res.send('Olá mundo!')
-})
+
+// Montagem da rota livro 
+app.use('/livros', routerLivros);
 
 app.listen(port, () => {
     console.log(`Escutando a porta ${port}`)
---
+++ alfala_books/backend/routes/livrosRouter.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import { getLivros } from '../Controllers/livroController.js';
+const router = express.Router();
+
+
+router.get('/', getLivros)
+
+router.post('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo POST');
+});
+
+router.patch('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo PATCH');
+});
+
+router.delete('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo DELETE');
+});
+
+
+export default router;
+
+
